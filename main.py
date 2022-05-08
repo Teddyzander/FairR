16701c1887dc3fbb1e7c6d56312957a9c57fafe2
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-05-09 09:40:28 +1000

    Can initialise RobustnesMetric class

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from robust_metric.robust_metric import RobustMetric
+import data_util.fetch_data as data_util
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test = RobustMetric()
+    print(test.cat)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- robust_metric/robust_metric.py
+++ robust_metric/robust_metric.py
@@ -1,15 +1,15 @@
 import data_util.fetch_data as data_util
 
+
 class RobustMetric:
     """
     Holds methods and instances for analysising the robustness of a data set for a certain optimisation problem
     with a particular fairness metric
     """
 
-    def __int__(self, fair_metric, sens, data=None, target=None, model="SVC"):
-        if data is None:
-            self.data = data_util.fetch_adult_data()
-        else:
-            self.data, self.target, self. = data_util.prepare_data(data, target, sens)
+    def __init__(self):
+        self.data, self.target, self.sensitive, self.cat, self.bounds = data_util.fetch_adult_data()
 
-        self.
+
+if __name__ == '__main__':
+    test = RobustMetric()
