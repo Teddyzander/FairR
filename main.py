c86ff84deb711bd101b7dcb07cefc99b7395131a
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-05-09 11:18:14 +1000

    Can create baseline model

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from robust_metric.robust_metric import RobustMetric
-from fairlearn.datasets import fetch_adult
-import data_util.fetch_data as data_util
 
 if __name__ == '__main__':
     test = RobustMetric()
     test.problem_summary()
     test.split_data()
     test.problem_summary()
+    score = test.run_baseline()
+
+    print(score)
