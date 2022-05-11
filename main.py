42c430dd2d13b8c9ff94847fb1a0c9abf9fca131
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-05-12 08:06:15 +1000

    Plot data with mean error bars

--- main.py
+++ main.py
@@ -31,11 +31,20 @@ if __name__ == '__main__':
 
     test = np.load('data/fairness_banking_dp_full_nopre.npy')
 
+    name = ['Baseline', 'Pre-processing', 'In-processing', 'Post-processing']
+    colour1 = ['-r', '-g', '-b', '-k']
+    colour2 = ['red', 'green', 'blue', 'black']
     means = np.zeros((4, 21))
     mean_er = np.zeros((4, 21))
+    x = np.arange(0, 21)
     for i in range(0, 4):
         for j in range(0, len(means[0])):
             means[i, j] = np.mean(test[i, j])
             mean_er[i, j] = np.std(test[i, j]) / np.sqrt(len(test[i, j]))
+
+    for i in range(0, 4):
+        pyplot.plot(np.arange(0, 21), means[i], colour1[i], label=name[i])
+        pyplot.fill_between(x, means[i] - mean_er[i], means[i] + mean_er[i], alpha=0.25, color=colour2[i])
+    pyplot.show()
 
     print('\nEnd')
